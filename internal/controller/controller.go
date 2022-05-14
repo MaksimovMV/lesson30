6b0ecaad0982607990d196266ce93ca97bc96f35
MaksimovMV <m_maksimov@mail.ru>
2022-05-14 16:15:32 +0500

    structure fixed, Graceful Shutdown, mutex and test.http added

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,18 +1,267 @@
 package controller
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"lesson30/internal/usecase"
+	"io/ioutil"
+	"lesson30/internal/model"
+	"lesson30/internal/storage"
+	"net/http"
+	"strconv"
 )
 
-func Build(r *chi.Mux, uc *usecase.UseCase) {
+func Build(r *chi.Mux, s *storage.Storage) {
 
 	r.Use(middleware.Logger)
 
-	r.Post("/create", uc.Create)
-	r.Post("/make_friends", uc.Make)
-	r.Delete("/user", uc.User)
-	r.Get("/friends/{targetID}", uc.Friends)
-	r.Put("/{targetID}", uc.NewAge)
+	r.Post("/users", func(w http.ResponseWriter, r *http.Request) {
+		createUser(w, r, s)
+	})
+
+	r.Get("/users/{sourceID}", func(w http.ResponseWriter, r *http.Request) {
+		getUser(w, r, s)
+	})
+
+	r.Patch("/users/{sourceID}", func(w http.ResponseWriter, r *http.Request) {
+		putNewAge(w, r, s)
+	})
+
+	r.Delete("/users/{sourceID}", func(w http.ResponseWriter, r *http.Request) {
+		deleteUser(w, r, s)
+	})
+
+	r.Put("/users/{sourceID}/friends", func(w http.ResponseWriter, r *http.Request) {
+		makeFriends(w, r, s)
+	})
+
+	r.Get("/users/{sourceID}/friends", func(w http.ResponseWriter, r *http.Request) {
+		getFriends(w, r, s)
+	})
+
+	r.Delete("/users/{sourceID}/friends/{targetID}", func(w http.ResponseWriter, r *http.Request) {
+		deleteFriend(w, r, s)
+	})
+}
+
+type inputParams struct {
+	TargetID int `json:"target_id"`
+	Age      int `json:"age"`
+}
+
+type message struct {
+	Message string `json:"message"`
+}
+
+func readParams(w http.ResponseWriter, r *http.Request) (inputParams, error) {
+	var p inputParams
+
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return p, err
+	}
+	defer r.Body.Close()
+
+	if err := json.Unmarshal(content, &p); err != nil {
+		return p, err
+	}
+
+	return p, nil
+}
+func createUser(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
+	w.Header().Add("Content-Type", "application/json")
+	content, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+	defer r.Body.Close()
+
+	var u model.User
+
+	if err := json.Unmarshal(content, &u); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	userID, err := s.PutUser(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	result := message{"Username_" + strconv.Itoa(userID) + " was created"}
+	b, _ := json.Marshal(result)
+	w.Write(b)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
+	w.Header().Add("Content-Type", "application/json")
+
+	sourceID, err := strconv.Atoi(chi.URLParam(r, "sourceID"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	u, err := s.GetUser(sourceID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	b, _ := json.Marshal(u)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func makeFriends(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
+	w.Header().Add("Content-Type", "application/json")
+	sourceID, err := strconv.Atoi(chi.URLParam(r, "sourceID"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	p, err := readParams(w, r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	if err := s.MakeFriends(sourceID, p.TargetID); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	result := message{"Username_" + strconv.Itoa(p.TargetID) + " и username_" + strconv.Itoa(sourceID) + " теперь друзья"}
+	b, _ := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
+	w.Header().Add("Content-Type", "application/json")
+	sourceID, err := strconv.Atoi(chi.URLParam(r, "sourceID"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	if err := s.DeleteUser(sourceID); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	result := message{"Username_" + strconv.Itoa(sourceID) + " удален"}
+	b, _ := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func getFriends(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
+	w.Header().Add("Content-Type", "application/json")
+
+	sourceID, err := strconv.Atoi(chi.URLParam(r, "sourceID"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	friends, err := s.GetFriends(sourceID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	b, _ := json.Marshal(friends)
+	b, _ = json.Marshal(message{string(b)})
+	w.Write(b)
+}
+
+func putNewAge(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
+	w.Header().Add("Content-Type", "application/json")
+	p, err := readParams(w, r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	sourceID, err := strconv.Atoi(chi.URLParam(r, "sourceID"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	if err := s.PutNewAge(sourceID, p.Age); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	result := message{"Возраст пользователя Username_" + strconv.Itoa(sourceID) + " успешно обновлен"}
+	b, _ := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func deleteFriend(w http.ResponseWriter, r *http.Request, s *storage.Storage) {
+	w.Header().Add("Content-Type", "application/json")
+	sourceID, err := strconv.Atoi(chi.URLParam(r, "sourceID"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	targetID, err := strconv.Atoi(chi.URLParam(r, "targetID"))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	if err := s.DeleteFriend(sourceID, targetID); err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		b, _ := json.Marshal(message{err.Error()})
+		w.Write(b)
+		return
+	}
+
+	result := message{"Username_" + strconv.Itoa(targetID) + " и username_" + strconv.Itoa(sourceID) + " больше не друзья"}
+	b, _ := json.Marshal(result)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,22 +1,22 @@
 package storage
 
 import (
-	"bytes"
 	"fmt"
-	"lesson30/internal/user"
-	"strconv"
+	"lesson30/internal/model"
+	"sync"
 )
 
 type Storage struct {
-	Repository map[int]*user.User
+	Repository map[int]*model.User
 	UserID     int
+	sync.Mutex
 }
 
 func NewStorage() Storage {
-	return Storage{make(map[int]*user.User), 0}
+	return Storage{make(map[int]*model.User), 0, sync.Mutex{}}
 }
 
-func (s *Storage) GetUser(targetID int) (*user.User, error) {
+func (s *Storage) GetUser(targetID int) (*model.User, error) {
 	u, ok := s.Repository[targetID]
 	if !ok {
 		return nil, fmt.Errorf("пользователя с ID %v не существует", targetID)
@@ -24,13 +24,19 @@ func (s *Storage) GetUser(targetID int) (*user.User, error) {
 	return u, nil
 }
 
-func (s *Storage) PutUser(u user.User) (int, error) {
+func (s *Storage) PutUser(u model.User) (int, error) {
+	s.Lock()
+	defer s.Unlock()
+
 	s.UserID++
 	s.Repository[s.UserID] = &u
 	return s.UserID, nil
 }
 
 func (s *Storage) MakeFriends(sourceID int, targetID int) error {
+	s.Lock()
+	defer s.Unlock()
+
 	if sourceID == targetID {
 		return fmt.Errorf("ID пользователей совпадают")
 	}
@@ -50,18 +56,21 @@ func (s *Storage) MakeFriends(sourceID int, targetID int) error {
 			return fmt.Errorf("пользователь с ID %v уже есть в друзьях у пользователя c ID %v", sourceID, targetID)
 		}
 	}
-	sourceUser.Friends = append(sourceUser.Friends, targetID)
 
 	for _, f := range targetUser.Friends {
 		if f == sourceID {
 			return fmt.Errorf("пользователь с ID %v уже есть в друзьях у пользователя c ID %v", targetID, sourceID)
 		}
 	}
+
+	sourceUser.Friends = append(sourceUser.Friends, targetID)
 	targetUser.Friends = append(targetUser.Friends, sourceID)
 	return nil
 }
 
 func (s *Storage) DeleteUser(targetID int) error {
+	s.Lock()
+	defer s.Unlock()
 
 	u, err := s.GetUser(targetID)
 
@@ -83,24 +92,56 @@ func (s *Storage) DeleteUser(targetID int) error {
 	return nil
 }
 
-func (s *Storage) GetFriends(targetID int) (bytes.Buffer, error) {
-	b := bytes.Buffer{}
+func (s *Storage) GetFriends(targetID int) ([]int, error) {
+	s.Lock()
+	defer s.Unlock()
+
 	u, err := s.GetUser(targetID)
 	if err != nil {
-		return b, err
+		return nil, err
 	}
-	for _, f := range u.Friends {
-		friend := s.Repository[f]
-		b.Write([]byte("ID:" + strconv.Itoa(f) + " Имя: " + friend.Name + "\n"))
-	}
-	return b, nil
+	return u.Friends, nil
 }
 
 func (s *Storage) PutNewAge(targetID int, age int) error {
+	s.Lock()
+	defer s.Unlock()
+
 	u, err := s.GetUser(targetID)
 	if err != nil {
 		return err
 	}
 	u.Age = age
+	return nil
+}
+
+func (s *Storage) DeleteFriend(sourceID int, targetID int) error {
+	s.Lock()
+	defer s.Unlock()
+
+	sourceUser, err := s.GetUser(sourceID)
+	if err != nil {
+		return err
+	}
+
+	targetUser, err := s.GetUser(targetID)
+	if err != nil {
+		return err
+	}
+
+	for i, fID := range sourceUser.Friends {
+		if fID == targetID {
+			sourceUser.Friends = append(sourceUser.Friends[:i], sourceUser.Friends[i+1:]...)
+			break
+		}
+	}
+
+	for i, fID := range targetUser.Friends {
+		if fID == sourceID {
+			targetUser.Friends = append(targetUser.Friends[:i], targetUser.Friends[i+1:]...)
+			break
+		}
+	}
+
 	return nil
 }
